c39e8ecf42381dc1a460ff76e4fd1ba891242390
AtomicDonuts <Pascal98@hotmail.it>
2024-10-06 11:53:35 +0200

    first version of the documentatio

--- Assignment4/assignment_4.py
+++ Assignment4/assignment_4.py
@@ -1,53 +1,105 @@
 #!/usr/bin/python3
+"""
+Probability Density Function module developed for the Computing Methods course
+"""
 
 from loguru import logger
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline
 
-
 class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
+    """
+    Probability Density Function for a given set of points via scipy InterpolatedUnivariateSplie
 
-    def __init__(self, x, y): 
+    Parameters:
+            x : (N,), array_like
+
+            y : (N,), array_like
+    """
+
+    def __init__(self, x, y):
+        """
+        Constructor mmodule
+
+        Parameters:
+            x : (N,), array_like
+
+            y : (N,), array_like
+        """
         spline = InterpolatedUnivariateSpline(x, y)
-        norm = spline.integral(x.min(), x.max())
+        self.norm = spline.integral(x.min(), x.max())
         self._x = x
-        self._y = y / norm
+        self._y = y / self.norm
         super().__init__(self._x, self._y)
 
     def _grid(self):
-        return np.linspace(self._x.min(), self._x.max(), 250)
+        """
+        return a dense set of point for plots in the self._x domain
+        """
+        return np.linspace(self._x.min(), self._x.max(), 1000)
 
     def normalizzation(self):
+        """
+        return the normalizzation of the pdf, expected value is 1.
+        """
         return self.integral(self._x.min(), self._x.max())
 
     def _cumulative(self, val):
+        """
+        return cpf of the pdf evalueted in val
+        Parameters:
+                val: float
+        """
         return self.integral(self._x.min(), val)
 
     def _a_cumulative(self):
+        """
+        return an array of the cpf in the given domain of self._x
+        """
         return [self._cumulative(i) for i in self._grid()]
 
     def ppf(self,val):
+        """
+        return ppf of the pdf evalueted in val
+        Parameters:
+                val: float
+        """
         prob2 = InterpolatedUnivariateSpline(self._a_cumulative(),self._grid())
         return prob2(val)
-    
+
     def _a_ppf(self):
+        """
+        return an array of the ppf in the given domain of self._x
+        """
         return [self.ppf(y) for y in np.linspace(0., 1., 100)]
-    
+
     def random(self):
+        """
+        reutrn a pseudo-random number according to the pdf
+        """
         return self.ppf(np.random.random())
 
     def plot_pdf(self):
+        """
+        plot of the input set of points and the interpoleted spline
+        """
         plt.scatter(self._x, self._y, color="orange")
         plt.plot(self._grid(), self(self._grid()))
 
     def plot_cumulative(self):
+        """
+        plot of the cpf of the pdf
+        """
         plt.plot(self._grid(), [self._cumulative(i) for i in self._grid()])
-    
+
     def plot_ppf(self):
+        """
+        plot of the cpf of the pdf
+        """
         plt.plot(np.linspace(0., 1., 100),self._a_ppf())
 
-def triang(x):
+def triang(val):
     """
     Funzione che calcola il valore di una funzione triangolare strettamente positiva.
 
@@ -58,15 +110,15 @@ def triang(x):
             Il valore della funzione triangolare in corrispondenza di x.
     """
     # Condizioni per definire i tratti della funzione triangolare
-    if x < 0 or x > 10:
+    if val < 0 or val > 10:
         return 0
-    elif x <= 5:
-        return x / 5
+    elif val <= 5:
+        return val / 5
     else:
-        return (10 - x) / 5
+        return (10 - val) / 5
 
 
-def gauss(x, mu, sigma):
+def gauss(val, mu_val, sigma):
     """
     Calcola il valore di una gaussiana in un punto x, con media mu e deviazione standard sigma.
 
@@ -79,22 +131,26 @@ def gauss(x, mu, sigma):
             Il valore della gaussiana in corrispondenza di x.
     """
 
-    return (1 / (sigma * np.sqrt(2*np.pi))) * np.exp(-(x - mu)**2 / (2*sigma**2))
+    return (1 / (sigma * np.sqrt(2*np.pi))) * np.exp(-(val - mu_val)**2 / (2*sigma**2))
 
+def some_tests():
+    _rand_numb = np.random.random()
+    logger.debug(f"RE: {np.abs(np.abs(np.cos(_rand_numb))/pdf.norm - pdf(_rand_numb))/100.}")
+    logger.debug(f"Value of pdf in {_rand_numb}: {pdf(_rand_numb)}")
 
 if __name__ == "__main__":
     data_x = np.linspace(0., 10., 30)
-    #data_y = np.array([triang(i) for i in data_x])
-    #data_y = np.array([gauss(i,5,1) for i in data_x])
+    # data_y = np.array([triang(i) for i in data_x])
+    # data_y = np.array([gauss(i,5,1) for i in data_x])
     data_y = np.abs(np.cos(data_x))
     pdf = ProbabilityDensityFunction(data_x, data_y)
     logger.info(pdf.normalizzation())
-    pdf.plot_pdf()
-    plt.show()
-    pdf.plot_cumulative()
-    plt.show()
-    pdf.plot_ppf()
-    plt.show()
-    plt.hist([pdf.random() for i in range(0,10000)],bins = 40)
-    plt.show()
-  
+    # pdf.plot_pdf()
+    # plt.show()
+    # pdf.plot_cumulative()
+    # plt.show()
+    # pdf.plot_ppf()
+    # plt.show()
+    # plt.hist([pdf.random() for i in range(0,10000)],bins = 40)
+    # plt.show()
+    some_tests()
