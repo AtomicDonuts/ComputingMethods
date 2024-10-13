aac3a059618995ea8e59bb78399fc7bddf0b3010
Aelita <aelita@localhost.localdomain>
2024-10-13 13:01:06 +0200

    Some progress, almost to Rome

--- AssignmentA2/assignment_a2.py
+++ AssignmentA2/assignment_a2.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from loguru import logger
 import numpy as np
 
-# addautopep8
+# Da correggere tutti i typo di timestamp
 
 
 @dataclass
@@ -19,38 +19,84 @@ class Readings:
         '''Return the converted value of the adc to the physical voltage
         '''
         return 1.69 * self.adc + 0.420
-    
-    def __call__(self):
-        return f"Timestamp: {self.timestamp} Voltage: {self.voltage(self.adc)}"
+
+    # C'è un modo per tenersi sia questa cosa qui, che gli array?
+    def __str__(self):
+        return f"Timestamp: {self.timestamp}, ADC Value: {self.adc}, Voltage: {self.voltage()}"
+
+    def __getitem__(self, index):
+        _tuple = (self.timestamp, self.voltage())
+        return _tuple[index]
 
 
 class VoltageData:
 
     '''Simple interface to a set of voltage readings.
     '''
+    # adc should not be exposed
 
     def __init__(self, timestaps, adcs):
         if len(timestaps) != len(adcs):
             raise IndexError("timestamps and adcs must be the same lenght")
-        self.adcs = adcs
-        self.timestaps = timestaps
+        self.adcs = np.float64(adcs)
+        self.timestaps = np.float64(timestaps)
         self._readings = [Readings(x, y)
-                         for (x, y) in zip(self.timestaps, self.adcs)]
+                          for (x, y) in zip(self.timestaps, self.adcs)]
+        self.voltages = np.float64([_list[1] for _list in self._readings])
+        # self._iterator = iter(self._readings)
+
+    def from_path(self, file_path):
+        pass
 
     def __iter__(self):
         self._iterator = iter(self._readings)
         return self
 
     def __next__(self):
-        return next(self._iterator)
+        pino = next(self._iterator)
+        return np.array([pino[0], pino[1]])
 
     def __getitem__(self, index):
         return self._readings[index]
+
+    def __len__(self):
+        return len(self._readings)
+
+    def __str__(self):
+        _str = "index\t(timestamps,voltage)\n"
+        for i, lines in enumerate(self._readings):
+            # _str = _str + f"{i}\t{lines}\n"
+            _str = _str + f"{i}\t{(str(lines[0]),str(lines[1]))}"
+            if i < len(self._readings) - 1:
+                _str = _str + "\n"
+            # fix this shit with some real function^
+        return _str
+
+    def __call__(self):
+        pass
+
+    def plot(self):
+        pass
+
 
 if __name__ == "__main__":
     adc_data = np.arange(0., 10., 1)
     timestamps_data = np.arange(0., 10., 1)
     pino = VoltageData(timestamps_data, adc_data)
+    logger.info(
+        "VoltageData is iterable, and its elements can be called with the [] notation")
     for i in pino:
-        print(i)
-    
+        logger.info(f"{i}\t{i[0]}\t{i[1]}")
+    gino = pino[3]
+    logger.info(f"VoltageData[3] is {gino}")
+    logger.info(f"The timestamp of element 3 of VoltageData"
+                f"is {pino[3][0]} and its type is {type(pino[3][0])}")
+    logger.info(f"VoltageData.timestamps is {pino.timestaps}"
+                f"and its type is {type(pino.timestaps)}")
+    logger.info(
+        f"The third value of VoltageData.timestamp is {pino.timestaps[2]}")
+    logger.info(
+        f"VoltageData is sliceable, this are the even index terms {pino.timestaps[0::2]}")
+    logger.info(f"{len(pino)}")
+    logger.info("VoltageData is callable with prints")
+    print(pino)
