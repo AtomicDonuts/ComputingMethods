e3038eef454e3cb5ee74e78bcef5bd82b62f7136
AtomicDonuts <Pascal98@hotmail.it>
2024-10-26 14:21:36 +0200

    added bit rappresentation of integer

--- C_Ex/flags.c
+++ C_Ex/flags.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
+#define BYTE 8
 
-int main(void){
+void int2bit(int write_binary, int numb_of_byte)
+/* Write on screen the bit rappresentation of a intger number
+*/
+{
+    for (int i = numb_of_byte * BYTE; i > 0; i--)
+    {
+        if (!(i % 4))
+            printf(" ");
+        printf("%u", (write_binary >> (i - 1)) & 1);
+    }
+}
 
-    int flags = 65;   // 0011 1111
-    int mask = 64;    // 0100 0000 
+int main(void)
+{
+    int flags = 63; // 0011 1111
+    int mask = 64;  // 0100 0000
     bool isValidData;
     isValidData = flags & mask;
-    printf(isValidData?"True\n\n":"False\n\n");
-    int write_binary = 107;
-    for(int i = 0; i < 8;i++){
-        printf("%u\n",write_binary>>i);
+    printf(isValidData ? "True\n\n" : "False\n\n");
+    int numb_of_byte = 4;
+    int max = __INT32_MAX__;
+    printf("%d", max);
+    for (int bin = 1; bin < max;bin = bin * 2)
+    {
+        printf("\n%d\n", bin);
+        int2bit(bin, numb_of_byte);
+        if (bin == 0) break;
     }
     return 0;
 }
