62757f1dabdc10fa94c01e456b479204fac8b2a7
AtomicDonuts <Pascal98@hotmail.it>
2025-11-08 22:52:29 +0100

    fine di sabato sera

--- HEP/Assignment_2_cpp/fourvector.cpp
+++ HEP/Assignment_2_cpp/fourvector.cpp
@@ -8,14 +8,22 @@ FourVector::FourVector(double E, double Px, double Py, double Pz)
     m_Py = Py;
     m_Pz = Pz;
 }
-double FourVector::get_E() {  return m_E; }
-double FourVector::get_Px() { return m_Px; }
-double FourVector::get_Py() { return m_Py; }
-double FourVector::get_Pz() { return m_Pz; }
+double FourVector::get_E()  const {  return m_E; }
+double FourVector::get_Px() const { return m_Px; }
+double FourVector::get_Py() const { return m_Py; }
+double FourVector::get_Pz() const { return m_Pz; }
 void FourVector::set_E(double f_input) { m_E = f_input; }
 void FourVector::set_Px(double f_input) { m_Px = f_input; }
 void FourVector::set_Py(double f_input) { m_Py = f_input; }
 void FourVector::set_Pz(double f_input) { m_Pz = f_input; }
+
+FourVector::operator +(FourVector const &pino){
+    FourVector _output();
+    _output.set_E(m_E + pino.get_E());
+    _output.set_Px(m_Px + pino.get_Px());
+    _output.set_Py(m_Py + pino.get_Py());
+    _output.set_Pz(m_Pz + pino.get_Pz());
+}
 
 double FourVector::p3Squared()
 {
--- HEP/Assignment_2_cpp/fourvector.h
+++ HEP/Assignment_2_cpp/fourvector.h
@@ -13,11 +13,13 @@ private:
 public:
     // Constructors
     FourVector(double E, double Px, double Py, double Pz);
+    // Operators
+    FourVector operator + (FourVector const &pino);
     // Accessors
-    double get_E();
-    double get_Px();
-    double get_Py();
-    double get_Pz();
+    double get_E()  const;
+    double get_Px() const;
+    double get_Py() const;
+    double get_Pz() const;
     void set_E(double f_input);
     void set_Px(double f_input);
     void set_Py(double f_input);
